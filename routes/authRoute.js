1b6fe3743600240b942a10c8f7a55841ffb9bf1f
Harshkumar12014013 <101981203+Harshkumar12014013@users.noreply.github.com>
2023-07-11 17:50:10 +0530

    final code
    ecommerce app

---
+++ routes/authRoute.js
@@ -0,0 +1,45 @@
+import  express  from "express";
+import {forgotPasswordController, getAllOrdersController, getOrdersController, loginController, orderStatusController, registerController, testController, updateProfileController} from '../controllers/authController.js'
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+//routing
+router.post('/register',registerController)
+
+//login
+router.post('/login',loginController)
+
+//forgot password
+router.post('/forgot-password',forgotPasswordController)
+
+//test
+router.get('/test',requireSignIn,isAdmin,testController)
+
+//protected route
+router.get('/user-auth',requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true})
+})
+
+router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
+    res.status(200).send({ok:true})
+})
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
+
+export default router
